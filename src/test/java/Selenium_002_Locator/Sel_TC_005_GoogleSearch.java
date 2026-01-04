0814504e0b0c2b4b301952c9871b27a883079118
Shadab Shaikh <sshadab1104@gmail.com>
2026-01-04 18:49:39 +0530

    Selenium Day 1

---
+++ src/test/java/Selenium_002_Locator/Sel_TC_005_GoogleSearch.java
@@ -0,0 +1,19 @@
+package Selenium_002_Locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Sel_TC_005_GoogleSearch {
+    @Test
+    public void googleSearch(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        driver.findElement(By.name("q"))
+                .sendKeys("Selenium WebDriver");
+        driver.close();
+    }
+}
